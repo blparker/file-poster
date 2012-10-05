364a1be50810bedba6fcd8d33076d74b819d5f72
Bryan Parker <bryan.parker24@gmail.com>
2012-10-04 21:34:13 -0500

    added test server

---
+++ test/test_server.js
@@ -0,0 +1,37 @@
+
+var http = require('http'),
+    formidable = require('formidable'),
+    path = require('path');
+
+var server = http.createServer();
+
+server.on('request', function(req, res) {
+
+    if(req.method.toLowerCase() === 'post') {
+        var form = new formidable.IncomingForm();
+        var fields = [],
+            files = [];
+
+        form
+            .on('fileBegin', function(name, file) {
+                file.path = __dirname + '/test' + path.extname(file.name);
+            })
+            .on('field', function(field, value) {
+                fields.push([ field, value ]);
+            })
+            .on('file', function(field, file) {
+                files.push([ field, file ]);
+            })
+            .on('end', function() {
+                res.end();
+            });
+
+        form.parse(req);
+    }
+    else {
+        res.end();
+    }
+});
+
+server.listen(8080);
+
