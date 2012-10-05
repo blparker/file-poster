abacc1a835caee47f6c3a01fbf38c36e53f8c724
Bryan Parker <bryan.parker24@gmail.com>
2012-10-04 19:10:17 -0500

    formatting

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
 
 var postFile = require('../'),
-	url = require('url'),
-	assert = require('assert');
+    url = require('url'),
+    assert = require('assert');
 
 var options = url.parse('http://localhost:8080/upload');
 options['method'] = 'POST';
 
 postFile(__dirname + '/kitten.jpg', options, function(err, res) {
-	assert.equal(res.statusCode, 200);
-	assert.equal(err, null);
+    assert.equal(res.statusCode, 200);
+    assert.equal(err, null);
 });
 
